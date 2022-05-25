95fd3d76606ee6b147c34477755eb738ea0f353f
seungchanbaeg <mokdonjr@gmail.com>
2022-05-26 02:11:40 +0900

    contract, migration 작성 및 truffle-config geth 인스턴스 정보 입력

---
+++ README.md
@@ -0,0 +1,10 @@
+### 프로젝트 생성
+truffle init
+
+### 프로젝트 작성
+contract 디렉터리에 스마트 컨트랙트 작성
+migration 디렉터리에 스마트 컨트랙트 배포 스크립트 작성
+truffle-config 파일에 스마트 컨트랙트를 배포할 geth 인스턴스(ganache로 대체) 의 활성화된 RPC 정보
+
+### 컴파일
+truffle compile
---
+++ migrations/2_custom.js
@@ -0,0 +1,9 @@
+var hw = artifacts.require("First");
+
+var hw1 = artifacts.require("Second");
+
+module.exports = function(deployer) {
+    deployer.deploy(hw).then(function() {
+	return deployer.deploy(hw1, hw.address);
+    })
+}
